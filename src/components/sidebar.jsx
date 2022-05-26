538cb77a0ceada4973315331c26c0522683dda10
Dave Keller <davekeller@me.com>
2022-05-26 11:19:40 -0500

    saving tweaks

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -76,7 +76,7 @@ export default function sidebar (props) {
         </button>
         <button 
           onClick={() => props.onChangePage('planList')}
-          className="text-white hover:text-slate-200 space-y-1 border-l-4 border-white px-8">
+          className="text-white hover:text-slate-200 space-y-1 border-l-4 border-white px-6">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
           </svg>
