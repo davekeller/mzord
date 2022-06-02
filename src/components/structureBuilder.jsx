e3430853215381fb6958b092798ce1d67e3dce63
Dave Keller <davekeller@me.com>
2022-06-02 17:42:36 -0500

    plans grid columns

--- src/components/structureBuilder.jsx
+++ src/components/structureBuilder.jsx
@@ -4,8 +4,7 @@ import Path from './path.jsx';
 export default function structureBuilder() {
   return (
     <div>
-      <div className="grid grid-cols-4 gap-6 p-6 pb-8 card border-t-4 border-teal-400 rounded-t">
-        <div className='col-span-3'>
+      <div className="p-6 pb-8 card border-t-4 border-teal-400 rounded-t">
           <h1 className='text-sm font-semibold text-qpSlate-500'>
             Plan Forecast
           </h1>
@@ -13,38 +12,6 @@ export default function structureBuilder() {
           <p className='mt-3 text-qpSlate-500 text-xs italic'>
             Drag the slider to see your earnings per attainment.
           </p>
-        </div>
-        <div className="col-span-1">
-          <h1 className='text-sm font-semibold text-qpSlate-500'>
-            Key Indicators
-          </h1>
-          <ul className="mt-2 pt-4 border-t-2 space-y-4 border-qpSlate-100 font-semibold text-sm">
-            <li className="py-3 px-2 flex space-x-2 bg-qpSlate-100 rounded-lg leading-tight">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-qpSky" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
-              </svg>            
-              <p>
-                Incentivizes 125% Attainment
-              </p>
-            </li>
-            <li className="py-3 px-2 bg-qpSlate-100 rounded-lg flex space-x-2 leading-tight">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-qpSky" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
-              </svg>            
-              <p>
-                Incentivizes 130% OTE
-              </p>
-            </li>
-            <li className="py-3 px-2 bg-qpSlate-100 rounded-lg flex space-x-2 leading-tight">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-qpSky" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
-              </svg>            
-              <p>
-                Top 10% Sales Performer
-              </p>
-            </li>
-          </ul>
-        </div>
       </div>
 
       <div className='mt-8 col-span-full'>
--- src/components/title.jsx
+++ src/components/title.jsx
@@ -1,11 +1,17 @@
 
 export default function Title() {
   return (
-    <div>
       <div className='flex-between'>
-        <div>
-          <h1 className="text-2xl font-bold">Account Executive Saasy New Biz Plan</h1>
-          <p className="text-xs text-qpSlate-500">This is a description line about an AE plan that is focused on New Saas Accounts.</p>
+        <div className="flex items-center space-x-4">
+          <div className="p-2 rounded-full bg-qpSky">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
+            </svg>        
+          </div>
+          <div>
+            <h1 className="text-2xl font-bold">Account Executive Saasy New Biz Plan</h1>
+            <p className="text-xs text-qpSlate-500">This is a description line about an AE plan that is focused on New Saas Accounts.</p>
+          </div>
         </div>
         <ul className="flex font-medium space-x-8 text-xs text-qpSlate-500">
           <li className="flex space-x-2 items-center">
@@ -28,6 +34,5 @@ export default function Title() {
           </li>
         </ul>
       </div>
-    </div>
   )
 }
