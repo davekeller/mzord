2daffe2c1926dce522a17ab983d3c38e2acef89b
Dave Keller <davekeller@me.com>
2022-06-07 18:15:55 -0500

    tweakin

--- src/components/cta.jsx
+++ src/components/cta.jsx
@@ -1,7 +1,7 @@
 
 export default function Cta() {
   return (
-    <div className="flex justify-between items-end">
+    <div className="mb-12 flex justify-between items-end">
       
       <div className="flex space-x-4 items-center">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-amber-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
