f45974a2f9e0f2d8a0dbf034906cfbeefec0edf1
Dave Keller <davekeller@me.com>
2022-06-07 18:13:30 -0500

    add hover gradient to try btn

--- src/components/cta.jsx
+++ src/components/cta.jsx
@@ -15,7 +15,7 @@ export default function Cta() {
 
       
       <div>
-        <button class="btn bg-qpWater border-none shadow-sm rounded-md flex space-x-2 px-4 py-2">
+        <button class="btn bg-qpWater border-none shadow-sm rounded-md flex space-x-2 px-4 py-2 hover:bg-gradient-to-br hover:from-qpSky hover:to-qpGreen-200">
           <span>Try This Plan</span>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
