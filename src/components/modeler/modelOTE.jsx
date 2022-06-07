b16e27a064acc5d2465b88ed7a0d3df7d264f50f
Dave Keller <davekeller@me.com>
2022-06-07 13:12:44 -0500

    pay mix inputs

--- src/components/modeler/modelOTE.jsx
+++ src/components/modeler/modelOTE.jsx
@@ -83,18 +83,22 @@ export default function ModelOTE (props) {
             </p>
           </label>
           <div className="relative">
-            <input type="text" className="input border-white pl-9" name="onTarget" placeholder="60%" />
+            <input type="text" className="input border-white pl-9" name="onTarget" placeholder="84,000" />
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 absolute top-2.5 left-1.5 text-qpSlate-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
             </svg>
           </div>
         </div>
 
-        <div className="flex-center flex-col text-center">
-          <p className="label">Pay Mix</p>
-          <h2 className="text-2xl font-bold">
-            60 : 40
-          </h2>
+        <div>
+          <label className="flex-center flex-col">
+            <p className="label">
+              Pay Mix
+            </p>
+          </label>
+          <div className="relative">
+            <input type="text" className="input text-center border-qpSlate-600 text-xl bg-qpSlate-600 text-white" name="onTarget" placeholder="60:40" value="60:40"/>
+          </div>
         </div>
 
         <div>
@@ -109,7 +113,7 @@ export default function ModelOTE (props) {
             </p>
           </label>
           <div className="relative">
-            <input type="text" className="input border-white pl-9" name="onTarget" placeholder="60%" />
+            <input type="text" className="input border-white pl-9" name="onTarget" placeholder="56,000" />
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 absolute top-2.5 left-1.5 text-qpSlate-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
             </svg>
