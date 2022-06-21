0ac1d45093fe094e8e3888bacd2413519295d8d8
Dave Keller <davekeller@me.com>
2022-06-21 17:40:11 -0500

    plan insights img

--- src/components/insights.jsx
+++ src/components/insights.jsx
@@ -1,15 +1,18 @@
 
 export default function Insights() {
   return (
-    <div className='mt-8 mb-12 space-y-2 prose'>
-      <h2 className='font-bold text-lg'>Plan Insights</h2>
-      <div className='text-sm text-qpSlate-500'>
-        <p>
-          The most basic form of sales commissions are Single Rate Commissions, also called Flat or Fixed Rate Commissions. All these terms mean you earn a set, single percentage off the deals you close.
-        </p>
-        <p>
-          The most basic form of sales commissions are Single Rate Commissions, also called Flat or Fixed Rate Commissions. All these terms mean you earn a set, single percentage off the deals you close.
-        </p>
+    <div className='my-12 space-y-2 flex items-start space-x-12'>
+      <img className="w-96" src="/mzord/assets/imgs/guide.png" alt="placeholder" />
+      <div className="prose">
+        <h2 className='font-bold text-lg'>Plan Insights</h2>
+        <div className='text-sm text-qpSlate-500'>
+          <p>
+            The most basic form of sales commissions are Single Rate Commissions, also called Flat or Fixed Rate Commissions. All these terms mean you earn a set, single percentage off the deals you close.
+          </p>
+          <p>
+            The most basic form of sales commissions are Single Rate Commissions, also called Flat or Fixed Rate Commissions. All these terms mean you earn a set, single percentage off the deals you close.
+          </p>
+        </div>
       </div>
     </div>
   )
